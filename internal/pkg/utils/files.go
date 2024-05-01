56bbb415da230a178c556074ae377e8910da5195
Markas Korotkovas <mkorotkovas@yahoo.com>
2024-04-30 18:46:04 -0600

    added the files functionality

---
+++ internal/pkg/utils/files.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+  "os"
+  "strings"
+  "bufio"
+)
+
+
+func GetWordsFromFile(numOfWords int, fileName string) string {
+  file, err := os.Open("words/" + fileName)
+  if err != nil {
+    println("Error opening file" + err.Error())
+  }
+  scanner := bufio.NewScanner(file)
+  scanner.Split(bufio.ScanLines)
+  var res strings.Builder
+  //TODO add randomness to this
+  for i := 0; i < numOfWords; i++ {
+    success := scanner.Scan()
+    if success == false {
+      break
+    }
+    res.WriteString(scanner.Text())
+  }
+  return res.String()
+}
