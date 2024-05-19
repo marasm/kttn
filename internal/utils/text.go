7e33b677c12575a3b0a3eb92111a6a6fb7d7c023
Markas Korotkovas <mkorotkovas@yahoo.com>
2024-05-19 14:31:35 -0600

    fixed a bug where the last word of the set would break in the middle

--- internal/utils/text.go
+++ internal/utils/text.go
@@ -3,7 +3,7 @@ package utils
 func DistanceToNextWhitespace(curPos int, textAsRunes []rune) int {
   res := 0
   for i, r := range textAsRunes[curPos + 1:] {
-    if r == ' ' {
+    if r == ' ' || curPos + i >= len(textAsRunes) - 2 {
       res = i + 1
       break
     }
